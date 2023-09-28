b36652dd07725f378816379f7933ec51ce774396
jquinain787 <141972848+jquinain787@users.noreply.github.com>
2023-09-27 19:35:51 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,10 @@
  * @param {string} fahren temperature in degrees F
  * @returns {number} the number of degrees C
  */
-function convertToCelsius(fahren) {}
+function convertToCelsius(fahren) {
+  //Return F - 32 * (5 / 9)
+  return (fahren - 32) * (5 / 9);
+}
 
 /**
  * Takes both numbers (F, C) and display a message with
@@ -22,27 +25,42 @@ function convertToCelsius(fahren) {}
  * @param {number} fahren
  * @param {number} celc
  */
-function createMessage(fahren, celc) {}
+function createMessage(fahren, celc) {
+  if (celc < 32) {
+    return(celc + " degrees celcius is very cold");
+  }
+
+  if (celc < 64 && celc > 32)  {
+    return(celc + " degrees celcius is cold");
+  }
+
+  if (celc < 86 && celc > 64)  {
+    return(celc + " degrees celcius is warm");
+  }
+
+  else {
+    return(celc + " degrees celcius is hot");
+  }
+
+}
 
 /**
  * Takes a number and returns a random integer from 0 to the limit
  * @param {number} limit
  * @returns {number} a number between 0 and the int passed in
  */
-function rand(limit) {}
+function rand(limit) {
+  return Math.floor(Math.random() * limit);
+}
 
 // -------------------- DONT NOT CHANGE THE CODE BELOW ---------------------- //
 
-let fahren = prompt(
-  "enter a number, we will convert that number from fahrenheit to celcius"
-);
+let fahren = prompt("enter a number, we will convert that number from fahrenheit to celcius");
 let celc = convertToCelsius(fahren);
 let output = createMessage(fahren, celc);
 console.log(output);
 
-fahren = prompt(
-  "Lets try that again. enter a number, we will convert that number from fahrenheit to celcius"
-);
+fahren = prompt("Lets try that again. enter a number, we will convert that number from fahrenheit to celcius");
 celc = convertToCelsius(fahren);
 output = createMessage(fahren, celc);
 console.log(output);
